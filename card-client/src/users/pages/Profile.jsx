0f6064bee9e1ed52f414cecfc339d8d07f753774
Tamir Yamaleyev <54142416+TamirYamaleyev@users.noreply.github.com>
2024-03-11 21:06:38 +0200

    Update Profile.jsx

--- card-client/src/users/pages/Profile.jsx
+++ card-client/src/users/pages/Profile.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
-import { getUserData } from "../services/usersApiService";
 
 export default function Profile() {
   const getUser = async () => {
     try {
-      const userData = await getUserData();
     } catch (error) {
     }
   };
